9e1e77e96ba6f9e64f3492c7d2d9182109b1df0f
davi109 <davi072011@gmail.com>
2021-08-28 17:46:16 -0300

    Adicionando controle de usuarios

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,9 @@ const CaControllerAP = require('./apside/caController')
 const CaController = require('./userside/caController')
 
 routes.post('/api/v1/user', jwt.login, UserController.create)
+routes.get('/api/v1/user', jwt.login, UserController.index)
+routes.put('/api/v1/user/:id', jwt.login, UserController.update)
+routes.delete('/api/v1/user/:id', jwt.login, UserController.delete)
 routes.get('/api/v1/verifica_login', jwt.verify_login)
 routes.post('/api/v1/login', UserController.login)
 
@@ -23,7 +26,7 @@ routes.post('/api/v1/group', jwt.login, GroupController.create)
 routes.put('/api/v1/group/:id', jwt.login, GroupController.update)
 routes.delete('/api/v1/group/:id', jwt.login, GroupController.delete)
 
-routes.get('/api/v1/ca', jwt.login, CaController.get)
+routes.get('/api/v1/ca', jwt.login, CaController.index)
 routes.put('/api/v1/ca', jwt.login, CaController.update)
 routes.delete('/api/v1/ca/:id', jwt.login, CaController.delete)
 
